8372698ad21498798480c7136359557d0f4d9a8d
Sean <5280104+seancl@user.noreply.gitee.com>
2019-11-11 11:47:41 +0800

    项目初始化

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// 导入字体文件
+
+// 最后导入我们自己的样式文件
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
